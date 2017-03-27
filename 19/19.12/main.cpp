bd7d70acf0d356456554e858128aea322613c876
lianlian <kuliantnt@gamil.com>
2017-03-27 09:09:48 +0800

    test

--- 19/19.12/main.cpp
+++ 19/19.12/main.cpp
@@ -1,7 +1,10 @@
 //
 // Created by lianlian on 17-3-27.
 //
-
+#include <iostream>
+#include "Screen.h"
 int main(int argc, char * argv[]){
-
+    Screen myScreen;
+    const std::string::size_type Screen::*pdate = Screen::date();
+    std::cout << myScreen.*pdate;
 }
