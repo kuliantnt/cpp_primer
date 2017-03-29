4aef7df40224062bea81b70086778f803c2818c7
lianlian <kuliantnt@gmail.com>
2017-03-29 20:01:42 +0800

    19.12

--- 19/19.21/Token.h
+++ 19/19.21/Token.h
@@ -6,7 +6,25 @@ class Token
 public:
     // 构造函数
     Token();
+    //析构函数
     ~Token();
+    //移动构造函数
+    Token(Token &&t);
+    //拷贝构造函数
+    Token(const Token &t) : tok(t.tok)
+    {
+        copyUnion(t);
+    }
+    //移动赋值运算符
+    Token& operator=(Token&&);
+    //拷贝赋值运算符
+    Token& operator= (const Token&&);
+    // 赋值运算符
+    Token& operator= (int i);
+    Token& operator= (char c);
+    Token& operator= (double d);
+    Token& operator= (std::string str);
+
 private:
     enum { INT, CHAR, DLB, STR }tok;
     union {
